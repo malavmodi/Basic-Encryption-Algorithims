7a86af63e32427bc577824e7cd76a39dcd0054a8
Malav Modi <mhm114@scarletmail.rutgers.edu>
2019-06-27 16:49:46 -0400

    almost finished decode
    now it is only converting the binary to actual characters

--- base_64.py
+++ base_64.py
@@ -5,31 +5,60 @@ base64_dict = {"0":"A", "1":"B", "2":"C", "3":"D", "4":"E", "5":"F", "6":"G", "7
 "26":"a", "27":"b", "28":"c", "29":"d", "30":"e", "31":"f", "32":"g", "33":"h", "34":"i", "35":"j", "36":"k", "37": "l", "38":"m", "39":"n", "40":"o", "41":"p", "42":"q", "43":"r", "44":"s", "45":"t", 
 "46":"u", "47":"v", "48":"w", "49":"x", "50":"y", "51":"z", "52":"0", "53":"1", "54":"2", "55":"3", "56":"4", "57":"5", "58":"6", "59":"7", "60":"8", "61":"9", "62":"+", "63":"/"}
 
-test_input = "how utterly fascinating"
-test_input_list = list(test_input)
+base64_dict_decode = dict(map(reversed, base64_dict.items()))
 
-for i in range(0, len(test_input_list)):
-    test_input_list[i] = format(ord(test_input_list[i]), '08b')
+def base64_encode_string(test_input):
+    test_input_list = list(test_input)
 
-binary_string = ''.join(test_input_list)
-temp = textwrap.wrap(binary_string,6)
-last_element_length = len(temp[len(temp)-1])
+    for i in range(0, len(test_input_list)):
+        test_input_list[i] = format(ord(test_input_list[i]), '08b')
 
-if(last_element_length < 6):
-    print("True!")
-    last_element = temp[len(temp)-1]
+    binary_string = ''.join(test_input_list)
+    temp = textwrap.wrap(binary_string,6)
+    last_element_length = len(temp[len(temp)-1])
+
+    if(last_element_length < 6):
+        last_element = temp[len(temp)-1]
 
     for j in range(0, 6 - last_element_length):
         last_element += '0'
         temp[len(temp)-1] = last_element
         
-for k in range(0, len(temp)):
-    temp[k] = int(temp[k],2) 
-    temp[k] = base64_dict.get(str(temp[k]))
+    for k in range(0, len(temp)):
+        temp[k] = int(temp[k],2) 
+        temp[k] = base64_dict.get(str(temp[k]))
 
-equals = 6 - last_element_length
-for x in range(0, int(equals / 2)):
-    temp.append("=")
+    equals = 6 - last_element_length
+    for x in range(0, int(equals / 2)):
+        temp.append("=")
     
-binary_string = ''.join(temp
-print("The base64 encoding of the string",test_input, "is",binary_string)
+    binary_string = ''.join(temp)
+    print("The base64 encoding of the string",test_input, "is",binary_string)
+    return binary_string
+    
+def base64_decode_string(encoded_input):
+    encoded_input = list(encoded_input)
+    
+    for i in range(0, len(encoded_input)):
+        if(encoded_input[i] == "="):
+            encoded_input.remove(encoded_input[i])
+
+    for i in range(0, len(encoded_input)):
+        encoded_input[i] = format(int(base64_dict_decode.get(encoded_input[i])),'06b')
+
+    binary_string = ''.join(encoded_input)
+    temp = textwrap.wrap(binary_string,8)
+
+    for j in range(0,len(temp)):
+        last_element = temp[len(temp)-1]
+        if((len(last_element)) < 8):
+            temp.remove(last_element)
+        else:
+            continue
+            temp[j] = int(temp[j],2)
+            print(temp[j])
+
+    print(temp)
+
+encoded = base64_encode_string("Hello")
+base64_decode_string(encoded)
